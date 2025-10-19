77c94baf265147b6be405be54a49162da6838b42
Zelda Ferris <zferris@wpi.edu>
2025-10-19 12:39:52 -0400

    descritive stats

--- gaze_clusters.py
+++ gaze_clusters.py
@@ -10,3 +10,30 @@
 # filter out color and spacing conditions
 # left with NC and NS
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df_gaze = pd.read_csv('gaze_all_anon.csv')
+
+df_perf = pd.read_csv('behavioral_all_anon.csv')
+
+print(df_perf['TaskCorrect'].mean())
+
+df_gaze['timestamp'].hist(bins=60, alpha=0.7)
+plt.title("Histogram of 'Timestamps'")
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.show()
+
+df_gaze['x'].hist(bins=60, color='orange', alpha=0.7)
+plt.title("Histogram of 'x locations'")
+plt.xlabel("x location")
+plt.ylabel("Frequency")
+plt.show()
+
+df_gaze['y'].hist(bins=60, color='purple', alpha=0.7)
+plt.title("Histogram of 'y locations'")
+plt.xlabel("y location")
+plt.ylabel("Frequency")
+plt.show()
