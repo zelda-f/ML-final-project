7b015cf3bfd8e540d99c1b8817e07664fb01a586
Zelda Ferris <zferris@wpi.edu>
2025-10-25 16:52:04 -0400

    add quartiles

--- features.py
+++ features.py
@@ -31,3 +31,29 @@ gaze_df = add_gaze_count_latency(gaze_df)
 gaze_df = add_gaze_count_latency(gaze_df, aoi_col="AOI_LOSide")
 print(gaze_df['HOSide_GazeCount'].head())
 print(gaze_df['LOSide_GazeCount'].head())
+
+# Add time quartiles
+
+def assign_quartile(group):
+    """
+    Adds column "time_quartile" which is an int 1-4 that represents the time quartile of the timestamp column
+    """
+    total_time = group['task_resp.rt'].iloc[0]
+    q1, q2, q3 = total_time / 4, total_time / 2, 3 * total_time / 4
+
+    def get_quartile(ts):
+        if ts <= q1:
+            return 1
+        elif ts <= q2:
+            return 2
+        elif ts <= q3:
+            return 3
+        else:
+            return 4
+
+    group['time_quartile'] = group['timestamp'].apply(get_quartile)
+    return group
+
+gaze_df = gaze_df.groupby(['Participant_anon', 'Problem_id'], group_keys=False).apply(assign_quartile)
+
+print(gaze_df['time_quartile'].head())
